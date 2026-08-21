c17fba2effe7384116fed0ef268233a463cce665
rayyanhatif-creator <rayyanhatif@gmail.com>
2026-08-21 05:04:33 +0300

    Add function to toggle menu display

---
+++ app.js
@@ -0,0 +1,18 @@
+function openMenu(){
+
+let menu = document.getElementById("menuList");
+
+
+if(menu.style.display === "block"){
+
+menu.style.display="none";
+
+}
+
+else{
+
+menu.style.display="block";
+
+}
+
+}
